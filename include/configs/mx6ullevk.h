bb9fe4a9497c5f249419d417b91c5eb6053872c2
wanguo <guohaoprc@163.com>
2024-12-19 16:26:13 +0800

    - Issues: Add uboot cmd sdboot and emmcboot - Date: 2024/12/19 16:26:13 - Author: wanguo

--- include/configs/mx6ullevk.h
+++ include/configs/mx6ullevk.h
@@ -23,6 +23,43 @@
 #define CFG_SYS_FSL_USDHC_NUM	2
 #endif
 
+/* user env Configs */
+#define CFG_EXTRA_USER_ENV_SETTINGS \
+	"ethaddr=00:11:22:33:44:55\0" \
+	"eth1addr=01:11:22:33:44:55\0" \
+	"ipaddr=10.10.0.221\0" \
+	"gatewayip=10.10.0.101\0" \
+	"serverip=10.10.0.201\0" \
+	"sddev=0\0" \
+	"sdpart=1\0" \
+	"dl_kernel=tftp ${loadaddr} ${image}; echo Download ${image} ok.\0" \
+	"dl_fdt=tftp ${fdt_addr} ${fdt_file}; echo Download ${fdt_file} ok.\0" \
+	"update_zImage=run dl_kernel;" \
+		"fatwrite mmc ${sddev}:${sdpart} ${loadaddr} ${image} ${filesize};" \
+		"echo update kernel ok.\0" \
+	"update_fdt=run dl_fdt;"\
+		"fatwrite mmc ${sddev}:${sdpart} ${fdt_addr} ${fdt_file} ${filesize};" \
+		"echo update fdt ok.\0" \
+	"dk=run findfdt; mmc dev ${sddev}; run update_zImage; run update_fdt\0" \
+	"sdroot=/dev/mmcblk0p2 rootwait rw\0" \
+	"sdargs=setenv bootargs console=${console},${baudrate} root=${sdroot}\0" \
+	"boot_with_tftp=yes\0" \
+	"emmcboot=echo Booting from eMMC Card ...; " \
+		"mmc dev ${mmcdev}; run mmcargs; " \
+		"run findfdt; run loadfdt; run loadimage; " \
+		"bootz ${loadaddr} - ${fdt_addr};\0" \
+	"sdboot=echo Booting from SD Card ...; " \
+		"mmc dev ${sddev}; run sdargs; " \
+		"if test ${boot_with_tftp} = 1; then " \
+			"echo Boot with tftp; " \
+			"run dl_kernel; run dl_fdt; " \
+		"else " \
+			"echo Boot without tftp; " \
+			"run findfdt; run loadfdt; run loadimage; " \
+		"fi; " \
+		"bootz ${loadaddr} - ${fdt_addr};\0" \
+
+
 #define CFG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"image=zImage\0" \
@@ -95,25 +132,7 @@
 		"else " \
 			"bootz; " \
 		"fi;\0" \
-	"ethaddr=00:11:22:33:44:55\0" \
-	"eth1addr=01:11:22:33:44:55\0" \
-	"ipaddr=10.10.0.221\0" \
-	"serverip=10.10.0.201\0" \
-	"gatewayip=10.10.0.101\0" \
-	"zImage_addr=0x82000000\0" \
-	"dl_zImage=tftp ${zImage_addr} zImage; echo Download zImage ok.\0" \
-	"dl_fdt=tftp ${fdt_addr} ${fdt_file}; echo Download ${fdt_file} ok.\0" \
-	"update_zImage=run dl_zImage;" \
-		"fatwrite mmc ${mmcdev}:${mmcpart} ${zimage_addr} /zImage ${filesize};" \
-		"echo update zImage ok.\0" \
-	"update_fdt=run dl_fdt;"\
-		"fatwrite mmc ${mmcdev}:${mmcpart} ${fdt_addr} /${fdt_file} ${filesize};" \
-		"echo update fdt ok.\0" \
-	"dk=run findfdt; mmc dev ${mmcdev}; run update_zImage; run update_fdt\0" \
-	"sdroot=/dev/mmcblk0p2 rootwait rw\0" \
-	"sdargs=setenv bootargs console=${console},${baudrate} root=${sdroot}\0" \
-	"sdboot=echo Booting from sd card ...;" \
-	"run sdargs; run dl_zImage; run dl_fdt; bootz ${zImage_addr} - ${fdt_addr}\0"
+	CFG_EXTRA_USER_ENV_SETTINGS
 
 /* Miscellaneous configurable options */
 
