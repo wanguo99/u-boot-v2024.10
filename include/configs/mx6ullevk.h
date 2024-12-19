d85b800d97c41bae349ec900e907df138484317f
wanguo <guohaoprc@163.com>
2024-12-19 20:58:00 +0800

    - Issues: Support run up cmd for updating the whole sdcard image - Date: 2024/12/21 15:27:58 - Author: wanguo

--- include/configs/mx6ullevk.h
+++ include/configs/mx6ullevk.h
@@ -23,41 +23,52 @@
 #define CFG_SYS_FSL_USDHC_NUM	2
 #endif
 
-/* user env Configs */
+/* Fix missing semicolon in write_file_to_mmc */
 #define CFG_EXTRA_USER_ENV_SETTINGS \
+	"tftp_kernel=1\0" \
+	"sector_size=0x200\0" \
+	"update_file=sdcard.img\0" \
+	"download_file=tftp ${dl_addr} ${dl_file}; " \
+		"if test $? -ne 0; then echo Error: Download ${dl_file} failed.; exit 1; fi; " \
+		"echo Download ${dl_file} OK.; " \
+		"setexpr temp ${filesize} + ${sector_size}; setexpr temp ${temp} - 1; " \
+		"if test ${sector_size} -lt ${temp}; then setexpr file_sectors ${temp} / ${sector_size}; " \
+		"else setenv file_sectors 1; fi; \0" \
+	"write_file_to_mmc=mmc dev ${sddev}; " \
+		"if test $? -ne 0; then echo Error: MMC device selection failed.; exit 1; fi; " \
+		"mmc write ${write_addr} ${write_offset} ${file_sectors}; " \
+		"if test $? -ne 0; then echo Error: MMC write failed at offset ${offset}.; exit 1; fi; " \
+		"mmc rescan; \0" \
+	"up=setenv dl_addr ${loadaddr}; setenv dl_file ${update_file}; " \
+		"run download_file; if test $? -ne 0; then exit 1; fi; " \
+		"setenv write_addr ${loadaddr}; setenv write_offset 0; run write_file_to_mmc; \0" \
 	"emmcboot=echo Booting from eMMC Card ...; " \
-		"mmc dev ${mmcdev}; run mmcargs; " \
-		"run findfdt; run loadfdt; run loadimage; " \
-		"bootz ${loadaddr} - ${fdt_addr};\0" \
-	"boot_with_tftp=1\0" \
+		"mmc dev ${mmcdev}; if test $? -ne 0; then echo Error: MMC device selection failed; exit 1; fi; " \
+		"run mmcargs; if test $? -ne 0; then echo Error: Setting MMC args failed; exit 1; fi; " \
+		"run findfdt; if test $? -ne 0; then echo Error: FDT not found; exit 1; fi; " \
+		"run loadfdt; if test $? -ne 0; then echo Error: Loading FDT failed; exit 1; fi; " \
+		"run loadimage; if test $? -ne 0; then echo Error: Loading kernel image failed; exit 1; fi; " \
+		"bootz ${loadaddr} - ${fdt_addr}; if test $? -ne 0; then echo Error: Kernel boot failed; exit 1; fi;\0" \
 	"sddev=0\0" \
-	"sdpart=1\0" \
 	"sdroot=/dev/mmcblk0p2 rootwait rw\0" \
 	"sdargs=setenv bootargs console=${console},${baudrate} root=${sdroot}\0" \
 	"sdboot=echo Booting from SD Card ...; " \
-		"mmc dev ${sddev}; run sdargs; " \
-		"if test ${boot_with_tftp} = 1; then " \
-			"echo Boot with tftp; " \
-			"run dl_kernel; run dl_fdt; " \
+		"mmc dev ${sddev}; if test $? -ne 0; then echo Error: SD device selection failed; exit 1; fi; " \
+		"run sdargs; if test $? -ne 0; then echo Error: Setting SD args failed; exit 1; fi; " \
+		"run findfdt; if test $? -ne 0; then echo Error: FDT not found; exit 1; fi; " \
+		"if test ${tftp_kernel} = 1; then " \
+			"echo Load kernel image from TFTP Server...; " \
+			"setenv dl_addr ${loadaddr}; setenv dl_file ${image}; " \
+			"run download_file; if test $? -ne 0; then exit 1; fi; " \
+			"setenv dl_addr ${fdt_addr}; setenv dl_file ${fdt_file}; " \
+			"run download_file; if test $? -ne 0; then exit 1; fi; " \
 		"else " \
-			"echo Boot without tftp; " \
-			"run findfdt; run loadfdt; run loadimage; " \
+			"echo Load kernel image from SD Card...; " \
+			"run loadfdt; if test $? -ne 0; then echo Error: Loading FDT failed; exit 1; fi; " \
+			"run loadimage; if test $? -ne 0; then echo Error: Loading kernel image failed; exit 1; fi; " \
 		"fi; " \
-		"bootz ${loadaddr} - ${fdt_addr};\0" \
-	"dl_kernel=tftp ${loadaddr} ${image}; tftp ${fdt_addr} ${fdt_file};\0" \
-	"dk=mmc dev ${sddev}; run findfdt; run dl_kernel; " \
-		"fatwrite mmc ${sddev}:${sdpart} ${loadaddr} ${image} ${filesize}; " \
-		"echo update kernel ok; " \
-		"fatwrite mmc ${sddev}:${sdpart} ${fdt_addr} ${fdt_file} ${filesize}; " \
-		"echo update fdt ok;\0" \
-	"uboot_file=u-boot-dtb.imx\0" \
-	"uboot_addr=0x84000000\0" \
-	"boot_start_sector=16384\0" \
-	"sector_size=0x200\0" \
-	"du=tftp ${uboot_addr} ${uboot_file}; " \
-		"setexpr file_sectors ${filesize} / ${sector_size}; " \
-		"mmc write ${uboot_addr} ${boot_start_sector} ${file_sectors}; " \
-		"echo Update U-Boot OK.\0"
+		"bootz ${loadaddr} - ${fdt_addr}; if test $? -ne 0; then echo Error: Kernel boot failed; exit 1; fi;\0"
+
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
@@ -106,6 +117,8 @@
 				"if test $fdt_file = undefined; then " \
 					"echo WARNING: Could not determine dtb to use; " \
 				"fi; " \
+				"echo setenv fdt_file ${fdt_file}; " \
+			"else exit 0; " \
 			"fi;\0" \
 	"netargs=setenv bootargs console=${console},${baudrate} " \
 		"root=/dev/nfs " \
